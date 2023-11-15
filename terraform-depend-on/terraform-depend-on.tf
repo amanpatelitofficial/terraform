cc67ac944acc5de83a3026848ba09a62b888596b
amanpatel.itofficial <amanpatel.itofficial@gmail.com>
2023-11-15 21:23:02 +0530

    terraform-depend-on

---
+++ terraform-depend-on/terraform-depend-on.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "my_test_bucket" {
+  bucket = "test-bucket-2-for-import"
+  tags = {
+    "name" = "test-bucket"
+  }
+}
+
+resource "aws_instance" "ec2_example" {
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  "t2.micro"
+
+   tags = {
+           Name = "Terraform EC2"
+   }
+  depends_on = [aws_s3_bucket.my_test_bucket]
+}
