83be6bbc78925f11b0d236b59b92d13c0fe17ff2
amanpatel.itofficial <amanpatel.itofficial@gmail.com>
2023-11-16 11:48:22 +0530

    SecretsHandling

---
+++ AWSecretsHandling/secrethandling.tf
@@ -0,0 +1,26 @@
+# 1st method
+
+provider "aws" {
+   profile    = "test"
+   region     = "eu-central-1"
+   access_key = "A0LPA99DFKJ74HW0CPAGH0FNM3"
+   secret_key = "a1toUHEBHSU49348JjdssNicCwcT61DfzHtHtkWJYhgnN"
+}
+
+
+# 2nd Method
+
+# you need to install awscli 
+# cd /home/aman/.aws/credentials 
+
+provider "aws" {
+   profile    = "test"
+   region     = "eu-central-1"
+   shared_credentials_file = "/home/vagrant/.aws/credentials"
+}
+
+
+# 3rd Method
+
+export AWS_ACCESS_KEY_ID="A0LPA509NDFKJ74HW0CPAGH0FNM3" 
+export AWS_SECRET_ACCESS_KEY="a1toUHEBHSU404823JjdssNicCwcT61DfzHtHtkWJYhgnN"
