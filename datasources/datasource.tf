c5c4dcc8fc56c3c67bbb70788b7567b907c37611
aman <amanpatel.itofficial@gmail.com>
2023-11-28 17:29:16 +0530

    datasource

--- datasources/datasource.tf
+++ datasources/datasource.tf
@@ -1,36 +1,26 @@
-
-/* In Terraform, data sources are used to fetch information or data from an external source, such as an existing infrastructure, and use that data within your Terraform configuration. Data sources are defined in a separate Terraform file with a .tf extension
-
-
-
-
-provider "aws" {
-    region     = "eu-central-1"
-    access_key = "AKIATQ37NXB2JMXVGYPG"
-    secret_key = "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"
+resource "aws_instance" "web" {
+     ami = data.aws_ami.web.id 
+     instance_type = "t2.micro"
+     
+     tags = {
+       Name = "web"
+     }
+      
+}
+output "web_ami_id" {
+  value = data.aws_ami.web.id
 }
 
-resource "aws_instance" "ec2_example" {
 
-    ami           = "ami-0767046d1677be5a0"
-    instance_type =  "t2.micro"
 
-    tags = {
-      Name = "Terraform EC2"
-    }
-}
+data "aws_ami" "web" {
+    most_recent = true
+    owners = [ "amazon" ]
 
-data "aws_instance" "myawsinstance" {
     filter {
-        name = "tag:Name"
-        values = ["Terraform EC2"]
+      name = "name"
+      values = ["Amazon Linux 2023 AMI 2023.2.20231113.0 x86_64 HVM kernel-6.1"]
     }
-
-    depends_on = [
-      "aws_instance.ec2_example"
-    ]
+  
 }
 
-output "fetched_info_from_aws" {
-  value = data.aws_instance.myawsinstance.public_ip
-}
