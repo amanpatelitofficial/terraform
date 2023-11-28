bc39185deec90fe18b34a2f6a1d01e1c8c6acdcb
aman <amanpatel.itofficial@gmail.com>
2023-11-28 17:56:11 +0530

    dependson

--- terraform-depend-on/terraform-depend-on.tf
+++ terraform-depend-on/terraform-depend-on.tf
@@ -1,16 +1,14 @@
-resource "aws_s3_bucket" "my_test_bucket" {
-  bucket = "test-bucket-2-for-import"
-  tags = {
-    "name" = "test-bucket"
-  }
+resource "aws_s3_bucket" "s1" {
+    bucket = "amanpatellinux"
+
+    tags = {
+      name = "s1"
+    } 
 }
 
-resource "aws_instance" "ec2_example" {
-   ami           = "ami-0767046d1677be5a0"
-   instance_type =  "t2.micro"
-
-   tags = {
-           Name = "Terraform EC2"
-   }
-  depends_on = [aws_s3_bucket.my_test_bucket]
+resource "aws_instance" "web" {
+    ami = "ami-0230bd60aa48260c6"
+    instance_type = "t2.micro"
+  
+  depends_on = [ aws_s3_bucket.s1 ]
 }
