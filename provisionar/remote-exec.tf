549f606521c613bb2e4d7b18c17b7d9c1f77089d
aman <amanpatel.itofficial@gmail.com>
2023-11-28 13:23:47 +0530

    provisinor

---
+++ provisionar/remote-exec.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "example" {
+  ami           = "ami-06aa3f7caf3a30282"
+  instance_type = "t2.micro"
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx",
+    ]
+    connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
+  }
+  }
+}
