b0122625023bc2da74c0cf1aa6816e7d64f00d8e
aman <amanpatel.itofficial@gmail.com>
2023-11-28 16:10:33 +0530

    provisinor

--- provisionar/remote-exec.tf
+++ provisionar/remote-exec.tf
@@ -1,28 +1,28 @@
-resource "aws_key_pair" "prod" {
-  key_name = "id_rsa_pub"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCw53OkIixHpy4n+P4lINRwArIBXxEMkBBBWWpRCa1WkQCYiKcF1iMpR37RSmDGZsyC2xXtiPJxwXOVycht7dBlCjWg11jgix8+IZUj6N0b7QLM/zZ/eR7LAV2ICD2kbxMYGM8vaGb/Zjv6aCl+wg8PRlkAGnYKGj7EfQL55JfcqINsfzBUS0hs5XLQUhaM6V79daLpxFitH202jEkOnr2piGx9y+w8THvPya2AePGtairOrtQ+wM9KXz3Bf4nDHfszobaSTe1cdA6b82Fksav9vvdEH+eUI5rJzbCInbSmQ3d34nlYD9+3fPDbKcZvkeGl5Etum+wNB1tASICoWwZ+vAcmx5T9AAC7sRzc2K5WR/sSp8u5ChthkXQTpr/9UU7cb3HmP+Fju4ix6mNnaUmg6nf0r6qWhhgOM05tghDjtvluIKxGj0T9PLaWr5Uz5WOPrmUp8Rbf+4JFTHiLpiEszmi3AqoaG5CmAqHt5apsLDlunobKgE6S1Qk95ba6v1k= root@ip-172-31-28-224"
-  
+resource "aws_key_pair" "web" {
+  key_name = "aws_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDV5nzsRFFHJA9ZqEqSpxjnewgJz9kLIYcblbdvXgGXnxUvEA+ksnoGhkQVVSrfNQP7b21m4bia8HGKUMNoEXeUSwRK3qYAH2h+RR+d8forX5J54fi0++5D4fyFddnVYmGxLQGpKMQjWolQ8fspqcNhJIjK/zQ/Rw6H2A7UKMzznvBaqYJyLfXFPPhIvkHs3sYPrHoR5jRLp5PE8F/nbSD0yHeS8MwdayOaWry9QddiyrULQAN7YwvD1nJO3Ik9eKFiFtBsJeOOBqjIHsNarOrMFZWhMlXusLjkVJdulznBsIpFfuhD5y2wT78HxrujoBA0SRjIBNUNWh1Y2iJ+v0t/etRW8hEsoQQC2d6QAe1fGgzzMnWlGF4e48jFIpew6exXZ2ToigF1U7LLh3wMDQKzbAftUs8Oi+pLpj0ULrM9bbEwyXPRj2rMf8sqaT/Ow6lg7+ubl3EoSwX0+yuR5P3iJMVPCIATdhR4hKnNhqid9RZbiB3dsI75QVtOmft46fc= root@ip-172-31-28-224"
 }
 
-
-
-
-resource "aws_instance" "example" {
-  ami           = "ami-06aa3f7caf3a30282"
+resource "aws_instance" "web" {
+  ami= "ami-0fc5d935ebf8bc3bc"
+  key_name = "aws_key"
   instance_type = "t2.micro"
-  key_name = "prod"
+  
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get install -y nginx",
-    ]
-    
-  }
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("~/.ssh/id_rsa")
-    host        = self.public_ip
-  }
+
+provisioner "remote-exec" {
+     inline = [
+      "sudo apt update",
+      "sudo apt install -y apache2* ",
+      
+     ]
+}
+
+     connection {
+      type= "ssh"
+      user = "ubuntu"
+      host = "self.public_ip"
+      private_key = file("./aws_key")
+     }  
+
 }
